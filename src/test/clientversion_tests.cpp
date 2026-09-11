7be48c3a6767536eda93b6f2ae525c91d4bed719
Blackcoin-Dev <298119138+Blackcoin-Dev@users.noreply.github.com>
2026-09-11 17:48:22 -0600

    test: use the historical 300105 numeric version in maintenance checks

--- src/test/clientversion_tests.cpp
+++ src/test/clientversion_tests.cpp
@@ -10,7 +10,7 @@ BOOST_AUTO_TEST_SUITE(clientversion_tests)
 
 BOOST_AUTO_TEST_CASE(maintenance_revision_preserves_numeric_compatibility)
 {
-    BOOST_CHECK_EQUAL(CLIENT_VERSION, 30105);
+    BOOST_CHECK_EQUAL(CLIENT_VERSION, 300105);
     BOOST_CHECK_EQUAL(CLIENT_VERSION_MAJOR, 30);
     BOOST_CHECK_EQUAL(CLIENT_VERSION_MINOR, 1);
     BOOST_CHECK_EQUAL(CLIENT_VERSION_BUILD, 5);
@@ -25,12 +25,12 @@ BOOST_AUTO_TEST_CASE(peer_revision_is_explicit_and_historical_format_is_unchange
     BOOST_CHECK_EQUAL(FormatSubVersion("Test", 99900, {}), "/Test:9.99.0/");
     BOOST_CHECK_EQUAL(FormatSubVersion("Test", 99900, {"comment1", "Comment2"}),
                       "/Test:9.99.0(comment1; Comment2)/");
-    BOOST_CHECK_EQUAL(FormatSubVersion("Blackcoin", 30105, {}, 0), "/Blackcoin:30.1.5/");
+    BOOST_CHECK_EQUAL(FormatSubVersion("Blackcoin", 300105, {}, 0), "/Blackcoin:30.1.5/");
     BOOST_CHECK_EQUAL(FormatSubVersion(CLIENT_NAME, CLIENT_VERSION, {}, CLIENT_VERSION_REVISION),
                       "/Blackcoin:30.1.5.1/");
     BOOST_CHECK_EQUAL(FormatSubVersion(CLIENT_NAME, CLIENT_VERSION, {"comment1", "Comment2"}, CLIENT_VERSION_REVISION),
                       "/Blackcoin:30.1.5.1(comment1; Comment2)/");
-    BOOST_CHECK_EQUAL(FormatSubVersion("Test", 30105, {}, 99), "/Test:30.1.5.99/");
+    BOOST_CHECK_EQUAL(FormatSubVersion("Test", 300105, {}, 99), "/Test:30.1.5.99/");
 }
 
 BOOST_AUTO_TEST_SUITE_END()
